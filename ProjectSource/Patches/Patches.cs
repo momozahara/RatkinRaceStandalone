bb29f739585c5a69121709168513b1db78c8b019
Pream Pinbut <momozahara@hotmail.com>
2024-09-07 15:57:49 +0700

    first commit

---
+++ ProjectSource/Patches/Patches.cs
@@ -0,0 +1,61 @@
+﻿using System.Xml;
+using Verse;
+
+namespace RatkinRaceStandalone.Patches
+{
+    public class RatkinPatchUseRatkinBodies : PatchOperationSequence
+    {
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            if (RatkinRaceStandaloneSettings.useRatkinBody)
+            {
+                Log.Message("RatkinPatchUseRatkinBodies Operation Apply");
+                return base.ApplyWorker(xml);
+            }
+            Log.Message("RatkinPatchUseRatkinBodies Operation Skipped");
+            return true;
+        }
+    }
+
+    public class RatkinPatchAllowInCoreFactions : PatchOperationSequence
+    {
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            if (RatkinRaceStandaloneSettings.allowRatkinInCoreFaction)
+            {
+                Log.Message("RatkinPatchAllowInCoreFactions Operation Apply");
+                return base.ApplyWorker(xml);
+            }
+            Log.Message("RatkinPatchAllowInCoreFactions Operation Skipped");
+            return true;
+        }
+    }
+
+    public class RatkinPatchAllowInEmpireFactions : PatchOperationSequence
+    {
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            if (RatkinRaceStandaloneSettings.allowRatkinInEmpireFaction)
+            {
+                Log.Message("RatkinPatchAllowInEmpireFactions Operation Apply");
+                return base.ApplyWorker(xml);
+            }
+            Log.Message("RatkinPatchAllowInEmpireFactions Operation Skipped");
+            return true;
+        }
+    }
+
+    public class RatkinPatchAllowInBiotechFactions : PatchOperationSequence
+    {
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            if (RatkinRaceStandaloneSettings.allowRatkinInBiotechFaction)
+            {
+                Log.Message("RatkinPatchAllowInBiotechFactions Operation Apply");
+                return base.ApplyWorker(xml);
+            }
+            Log.Message("RatkinPatchAllowInBiotechFactions Operation Skipped");
+            return true;
+        }
+    }
+}
---
+++ ProjectSource/RatkinRaceStandaloneMod.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace RatkinRaceStandalone
+{
+    public class RatkinRaceStandaloneSettings : ModSettings
+    {
+        public static bool useRatkinBody = true;
+        public static bool allowRatkinInCoreFaction = true;
+        public static bool allowRatkinInEmpireFaction = true;
+        public static bool allowRatkinInBiotechFaction = true;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref useRatkinBody, "useRatkinBody", true);
+            Scribe_Values.Look(ref allowRatkinInCoreFaction, "allowRatkinInCoreFaction", true);
+            Scribe_Values.Look(ref allowRatkinInBiotechFaction, "allowRatkinInBiotechFaction", true);
+            base.ExposeData();
+        }
+
+        public static void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+
+            listingStandard.Begin(inRect);
+            listingStandard.CheckboxLabeled("Use Ratkin body", ref useRatkinBody, "required restart to make change");
+            listingStandard.CheckboxLabeled("Allow Ratkin to spawn in core factions", ref allowRatkinInCoreFaction, "required restart to make change");
+            listingStandard.CheckboxLabeled("Allow Ratkin to spawn in empire factions", ref allowRatkinInEmpireFaction, "required restart to make change");
+            listingStandard.CheckboxLabeled("Allow Ratkin to spawn in biotech factions", ref allowRatkinInBiotechFaction, "required restart to make change");
+            listingStandard.End();
+        }
+    }
+
+    public class RatkinRaceStandaloneMod : Mod
+    {
+        RatkinRaceStandaloneSettings settings;
+
+        public RatkinRaceStandaloneMod(ModContentPack content) : base(content)
+        {
+            Log.Message("Ratkin Race Standalone loaded");
+
+            this.settings = GetSettings<RatkinRaceStandaloneSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            RatkinRaceStandaloneSettings.DoSettingsWindowContents(inRect);
+        }
+        public override string SettingsCategory()
+        {
+            return "Ratkin Race Standalone";
+        }
+    }
+}
